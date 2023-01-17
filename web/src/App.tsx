cca63d29abacd22f7f73550fed794fb08c086958
Vander-Reis <vanderreis2017@outlook.com>
2023-01-17 13:33:29 -0300

    first commit

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import "./styles/global.css"
+
+import { Habit } from "./components/Habit";
+
+export function App() {
+  return(
+    <div>
+      <Habit completed={3} />
+      <Habit completed={20} />
+      <Habit completed={30} />
+      <Habit completed={50} />
+    </div>
+  )
+}
+
