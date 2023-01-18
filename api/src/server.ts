5a02b2e8628f854e3ee830a1f65c67da1778b8d1
Vander-Reis <vanderreis2017@outlook.com>
2023-01-18 16:45:29 -0300

    criação das rotas na api / criação da home page no front

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,19 +1,16 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors"
-import { PrismaClient } from "@prisma/client"
+import { appRoutes } from './routes';
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
 app.register(cors);
 
-app.get("/", async () => {
-  const habits = await prisma.habit.findMany();
-  return habits;
-})
+app.register(appRoutes);
+
 
 app.listen({
   port: 8080,
 }).then(() => {
   console.log('Http Server running')
-})
+});
