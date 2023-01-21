273fe0b44a8571a166fc426948df46bfaca7c4e7
Vander-Reis <vanderreis2017@outlook.com>
2023-01-21 20:25:16 -0300

    conectando a API

--- api/src/server.ts
+++ api/src/server.ts
@@ -11,6 +11,7 @@ app.register(appRoutes);
 
 app.listen({
   port: 8080,
+  host: '192.168.18.65',
 }).then(() => {
   console.log('Http Server running')
 });
--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,12 +1,14 @@
-import { View, ScrollView, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, ScrollView, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
 import { BackButton } from '../components/BackButton';
 import { CheckBox } from '../components/CheckBox';
 import { useState } from 'react'
 import { Feather } from '@expo/vector-icons';
 import colors from 'tailwindcss/colors';
+import { api } from '../lib/axios';
 const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
 
 export function New() {
+  const [title, setTitle] = useState('');
   const [weekDays, setWeekDays] = useState<number[]>([])
 
   function handleToogleWeekDays(weekDayIndex: number) {
@@ -16,6 +18,25 @@ export function New() {
       setWeekDays(prevState => [...prevState, weekDayIndex]);
     }
   }
+
+  async function handleCreateNewHabit() {
+    try {
+      if(!title.trim() || weekDays.length === 0) {
+        Alert.alert('Novo Hábito', 'Informe o nome do hábito e escolha a periodicidade.')
+      }
+
+      await api.post('/habits', {title, weekDays})
+      setTitle('');
+      setWeekDays([]);
+
+      Alert.alert('Novo Hábito', 'Hábtiro criado com sucesso.')
+
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Ops', 'Não foi possível criar um novo hábito!')
+    }
+  }
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: 100 }}>
@@ -25,8 +46,11 @@ export function New() {
         <Text className='mt-6 text-white font-semibold text-base'>Qual seu comprometimento?</Text>
 
         <TextInput placeholder='Exercícios, dormir bem, etc...' 
-        placeholderTextColor={colors.zinc[400]}
-        className='h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600 '/>
+          placeholderTextColor={colors.zinc[400]}
+          className='h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600 '
+          onChangeText={setTitle}
+          value={title}
+        />
 
         <Text className='font-semibold mt-4 mb-3 text-white text-base'>
           Qual a recorrência?
@@ -41,8 +65,11 @@ export function New() {
         />
         ))}
 
-        <TouchableOpacity className='w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6' 
-        activeOpacity={0.7}>
+        <TouchableOpacity 
+          className='w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6' 
+          activeOpacity={0.7}
+          onPress={handleCreateNewHabit}
+        >
           <Feather name="check" size={20} color={colors.white}/>
           <Text className='font-semibold text-base text-white ml-2'>Confirmar</Text>
         </TouchableOpacity>
